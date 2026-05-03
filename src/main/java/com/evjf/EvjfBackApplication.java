188717573f397438cbb735781d1f6ac0f14f2dfa
Lena <iza.khabouri@gmail.com>
2026-05-03 23:57:26 +0200

    feat(init): create structure back project

---
+++ src/main/java/com/evjf/EvjfBackApplication.java
@@ -0,0 +1,13 @@
+package com.evjf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvjfBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvjfBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evjf/EvjfBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evjf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvjfBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
