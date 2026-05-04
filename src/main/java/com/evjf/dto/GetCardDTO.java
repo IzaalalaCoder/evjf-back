c50cac7cf405e1bd96bba9a45e33d1e9c9938d78
Lena <iza.khabouri@gmail.com>
2026-05-04 20:49:11 +0200

    feat(createWebSocket): update GetCardDTO

--- src/main/java/com/evjf/dto/GetCardDTO.java
+++ src/main/java/com/evjf/dto/GetCardDTO.java
@@ -2,4 +2,4 @@ package com.evjf.dto;
 
 import com.evjf.enumerate.Level;
 
-public record GetCardDTO(int id, String question, String points, Level level) {}
+public record GetCardDTO(Integer id, String question, String points, Level level) {}
